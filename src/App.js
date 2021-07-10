820021f7a7e53a833965d9d039574db5d37dd3c9
KuchkarovShakhzod <kuchkarovsh99@gmail.com>
2021-07-10 14:19:32 +0500

    title changed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     alt = "logo" / >
     <
     p >
-    React bilan birinchi loyiha <
+    Edit < code > src / App.js < /code> and save to reload. <
     /p> <
     a className = "App-link"
     href = "https://reactjs.org"
